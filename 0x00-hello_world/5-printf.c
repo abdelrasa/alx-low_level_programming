d48739da24c063d99cae1ab9e40d6dda31bbf4e5
abdelrasa <applicationrasa@gmail.com>
2023-03-18 07:30:47 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <tsdio.h>
+/**
+ * main - A program use the function frintf
+ *
+ * Return: 0 (always)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
