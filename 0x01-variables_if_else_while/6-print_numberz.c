191e908e93b074eeb98d12e37dbf3b95b9a0152c
abdelrasa <applicationrasa@gmail.com>
2023-03-19 16:09:27 -0700

    nmbrs

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Single numbers with putchar
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
