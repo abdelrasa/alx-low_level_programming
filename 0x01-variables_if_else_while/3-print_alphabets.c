7f8272e2eeb0b12946f2c58ce532fc0559cf8f31
abdelrasa <applicationrasa@gmail.com>
2023-03-19 15:20:31 -0700

    prit alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet in lowercase
+ * followed by a new line
+ * Return: Always (0)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
