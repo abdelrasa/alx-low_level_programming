1e1f5c1450639afaa1e08b7f875fde4cd1a3be44
abdelrasa <applicationrasa@gmail.com>
2023-03-20 13:26:22 -0700

    task9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,12 +7,15 @@
 int main(void)
 {
 	int i;
+
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + 48);
-	
-	putchar(',');
-	putchar(' ');
+	if (i < 9)
+	{
+		putchar(',');
+		putchar(' ');
+}
 	}
 	putchar('\n');
 	return (0);
