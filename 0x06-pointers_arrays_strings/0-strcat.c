2c4abab3d89453c5965042af63eef5875cb54fa9
abdelrasa <applicationrasa@gmail.com>
2023-03-29 15:14:08 -0700

    task 00

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,6 @@
  */
 char *_strcat(char *dest, char *src)
 {
-
 	int i;
 	int j;
 
@@ -17,12 +16,14 @@ char *_strcat(char *dest, char *src)
 	{
 		i++;
 	}
+	j = 0;
 	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
