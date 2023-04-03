cc761c9ad4e15f428db560948da672de21cb4b8b
abdelrasa <applicationrasa@gmail.com>
2023-04-03 16:53:37 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - fill memory with value.
+ * @s: pointer
+ * @b: constant byte
+ * @n: number of the byte use
+ *
+ * Return: pointer n
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; n > 0; i++; n--)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme about part 0x07. C - Even more pointers, arrays and strings
