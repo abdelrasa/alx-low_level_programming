8aac2d5e9f5e1f629001ab2488e39a5596ff807b
abdelrasa <applicationrasa@gmail.com>
2023-04-04 15:37:39 -0700

    task__4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - function locates the first occurrence in the string
+ * @s: pointer
+ * @accept: bytes in the string
+ * Retrun: 0 Always
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
