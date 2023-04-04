8b3e4292f631bc42dce89a7a2b40febf7e60a818
abdelrasa <applicationrasa@gmail.com>
2023-04-04 16:12:40 -0700

    4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,16 +7,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i;
+		int i;
 
-	while (*s)
-	{
-		for (i = 0; accept[i]; i++)
+		while (*s)
 		{
+			for (i = 0; accept[i]; i++)
+			{
 			if (*s == accept[i])
 			return (s);
-		}
+			}
 		s++;
-	}
+		}
+
 	return ('\0');
 }
