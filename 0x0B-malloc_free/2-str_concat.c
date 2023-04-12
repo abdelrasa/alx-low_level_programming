c3d922421205394ac886f239c2930f163cbcb691
abdelrasa <applicationrasa@gmail.com>
2023-04-12 14:59:39 -0700

    2tsk

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * str_concat - concatenates two trings to find length
+ * @s1: string
+ * @s2: string
+ * Return: s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *cnt;
+	int i;
+	int j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	i = j = 0;
+	while (s1[i] != '\0')
+		i++
+	while (s1[j] != '\0')
+		j++
+
+	cnt = malloc(sizeof(char) * (i + ci + 1));
+
+	if (cnt == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		cnt[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		cnt[i] = s2[j];
+		i++;
+		j++;
+	}
+	cnt[i] = '\0';
+	return (cnt);
+}
