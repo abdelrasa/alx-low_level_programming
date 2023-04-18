f1d078a4dbd031a88b0ddfd7ca3e3086999897f7
abdelrasa <applicationrasa@gmail.com>
2023-04-17 17:50:16 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct name
+ * @name: member_1
+ * @age: member_2
+ * @owner: member_3
+ *
+ * Description: struct name about dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+#endif
