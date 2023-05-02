b7380ca9bd4698ef7be652cf6017c3b209cd46d9
abdelrasa <applicationrasa@gmail.com>
2023-05-02 13:46:34 -0700

    t0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,7 +7,6 @@
  *
  *Return: the number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t j;
@@ -16,9 +15,10 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		printf("%d \n", h->n);
-		h = h->next;
+		printf("%d\n", h->n);
 		j++;
+		h = h->next;
 	}
+
 	return (j);
 }
